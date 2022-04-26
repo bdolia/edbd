c20070db2b313380121af2afe45aead56f41f26b
Bogdan Dolia <59928849+BogdanDolia@users.noreply.github.com>
2022-04-26 12:36:16 +0300

    Update __init__.py

--- edbd/__init__.py
+++ edbd/__init__.py
@@ -25,7 +25,6 @@ class edbd(object):
 
 
     def encrypt_text(self):
-        
         for i in range(len(self.text)):
             if ord(self.text[i]) in self.bucket:
                 if self.bucket[ord(self.text[i])]:
@@ -34,42 +33,31 @@ class edbd(object):
                     self.encrypted_text.append(0)
             else:
                 self.encrypted_text.append(0)
-
         return self.encrypted_text
 
 
     def return_encrypted_text(self):
-
         encrypted_text = ""
         for i in self.encrypted_text:
             encrypted_text = encrypted_text + str(i) + " "
-        
         return encrypted_text[:-1]
 
 
     def decrypt_text(self):
-
         splited_text = self.text.split(" ")
-
         for i in range(len(splited_text)):
-
             found_char = False
-
             for char_unicode, position in self.bucket.items():
                 if int(splited_text[i]) in position:
                     self.decrypted_text.append(chr(char_unicode))
                     found_char = True
-            
             if not found_char:
                 self.decrypted_text.append(32)
-
         return self.decrypted_text
 
 
     def return_decrypted_text(self):
-
         decrypted_text = ""
         for i in self.decrypted_text:
             decrypted_text = decrypted_text + str(i)
-        
         return decrypted_text            

7471e7d3f29790665f04b1ba01f6ea30fd44d091
Bogdan Dolia <cr.co.erph@gmail.com>
2023-08-25 19:26:52 +0300

    requirements.txt bumped

--- setup.py
+++ setup.py
@@ -3,22 +3,21 @@
 from os import path
 from setuptools import setup
 
-
 wd = path.abspath(path.dirname(__file__))
 with open(path.join(wd, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    description = 'A text encryptor and decryptor that use a text file as a key.',
-    name = 'edbd',
-    version = '0.1.0',
-    author = 'Bogdan Dolia',
-    author_email = 'cr.co.erph@gmail.com',
+    description='A text encryptor and decryptor that use a text file as a key.',
+    name='edbd',
+    version='0.2.0',
+    author='Bogdan Dolia',
+    author_email='cr.co.erph@gmail.com',
     license='GNU General Public License v3.0',
-    url = 'https://github.com/BogdanDolia/edbd',
-    download_url = 'https://github.com/bdolia/edbd/archive/0.0.1.tar.gz',
+    url='https://github.com/BogdanDolia/edbd',
+    download_url='https://github.com/bdolia/edbd/archive/0.0.1.tar.gz',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    keywords = ['encrypt', 'decrypt'],
-    packages = ['edbd']
+    keywords=['encrypt', 'decrypt'],
+    packages=['edbd']
 )
